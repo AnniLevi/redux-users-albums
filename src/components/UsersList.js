bf12c583a6dd4fd09f837f8fe2401287ba2384da
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-02-20 22:47:35 +0100

    create useThunk custom hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,8 +1,64 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers, addUser } from "../store";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { addUser, fetchUsers } from "../store";
+import { useThunk } from "../hooks/useThunk";
 import Button from "./Button";
 import Skeleton from "./Skeleton";
+
+function UsersList() {
+  const [doFetchUsers, isLoadingUsers, loadingUsersError] =
+    useThunk(fetchUsers);
+  const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => {
+    return state.users;
+  });
+
+  useEffect(() => {
+    doFetchUsers();
+  }, [doFetchUsers]);
+
+  const handleUserAdd = () => {
+    doCreateUser();
+  };
+
+  if (isLoadingUsers) {
+    return <Skeleton times={6} className="h-10 w-full" />;
+  }
+  if (loadingUsersError) {
+    return <div>Error fetching data</div>;
+  }
+
+  const renderedUsers = data.map((user) => {
+    return (
+      <div key={user.id} className="mb-2 border rounded">
+        <div className="flex p-2 justify-between items-center cursor-pointer">
+          {user.name}
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        {isCreatingUser ? (
+          "Creating User..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && "Error creating user"}
+      </div>
+      {renderedUsers}
+    </div>
+  );
+}
+
+export default UsersList;
+
+/*
+without useThunk hook
 
 function UsersList() {
   // make tracking state by useState() instead of Redux
@@ -24,11 +80,11 @@ function UsersList() {
     return state.users;
   });
 
-  /*
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
-   */
+
+  // useEffect(() => {
+  //   dispatch(fetchUsers());
+  // }, [dispatch]);
+
 
   useEffect(() => {
     setIsLoadingUsers(true);
@@ -76,8 +132,12 @@ function UsersList() {
     <div>
       <div className="flex flex-row justify-between m-3">
         <h1 className="m-2 text-xl">Users</h1>
-        {isCreatingUser ? "Creating User..." : <Button onClick={handleUserAdd}>+ Add User</Button>}
-        {creatingUserError && 'Error creating user'}
+        {isCreatingUser ? (
+          "Creating User..."
+        ) : (
+          <Button onClick={handleUserAdd}>+ Add User</Button>
+        )}
+        {creatingUserError && "Error creating user"}
       </div>
       {renderedUsers}
     </div>
@@ -85,3 +145,5 @@ function UsersList() {
 }
 
 export default UsersList;
+
+ */
