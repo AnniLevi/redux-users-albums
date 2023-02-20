16f48ef8dfed4a88576af54647c0f52fae957992
Anna Levitskaya <anna.levitskaya@polcode.net>
2023-02-20 17:02:32 +0100

    create fetching users with React Async Thunk

---
+++ src/store/api/fetchUsers.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+// React Async Thunk for fetching a list of users from API
+
+// the first argument is a base type, it will be used to generate the action type which will be dispatched
+// the action type will be generated as 'users/fetch/pending' when initially make a request
+// and 'users/fetch/fulfilled' when data will be fetched successfully
+// in case of error occurred during the request type will be 'users/fetch/rejected'
+const fetchUsers = createAsyncThunk("users/fetch", async () => {
+  const response = await axios.get(`${apiUrl}/users`);
+
+  // dev only
+  // await pause(1000);  // 1 sec
+
+  return response.data;
+});
+
+
+// add a pause for development purposes
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
+
+export { fetchUsers };
--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,11 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchUsers } from "../api/fetchUsers";
 
 const usersSlice = createSlice({
   name: "users",
   initialState: {
     data: [],
+    isLoading: false,
+    error: null,
   },
-  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.pending, (state, action) => {
+      // fetchUsers.pending === 'users/fetch/pending'
+
+      // data is loading
+      state.isLoading = true;
+    });
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // request success
+      state.isLoading = false;
+      // action.payload === request.data from fetchUsers function
+      state.data = action.payload;
+    });
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      // error occurred with request
+      state.isLoading = false;
+      // in this case action.error is an error object
+      state.error = action.error;
+    });
+  },
 });
 
 export const usersReducer = usersSlice.reducer;
